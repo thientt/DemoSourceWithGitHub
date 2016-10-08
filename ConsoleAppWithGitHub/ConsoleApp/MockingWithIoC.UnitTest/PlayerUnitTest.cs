0900fee266b068d234e303be0c93cec237bec269
thientt <tranthiencdsp@gmail.com>
2016-10-08 11:17:10 +0700

    clean code with the support ReShaper

--- ConsoleAppWithGitHub/ConsoleApp/MockingWithIoC.UnitTest/PlayerUnitTest.cs
+++ ConsoleAppWithGitHub/ConsoleApp/MockingWithIoC.UnitTest/PlayerUnitTest.cs
@@ -9,13 +9,11 @@ namespace MockingWithIoC.UnitTest
         [TestMethod]
         public void PlayTest()
         {
-            var player = new Player();
-
             var mock = new Mock<IPlayer>();
             mock.Setup(x => x.Add()).Returns(true);
             mock.Setup(x => x.Play()).Returns(true);
 
-            SongMusic music = new SongMusic(mock.Object);
+            var music = new SongMusic(mock.Object);
             Assert.AreEqual(music.Play(), true);
         }
     }
