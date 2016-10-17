aa856e129f8c1d5cb1295fb0b18326bb4a9922c1
thientt <tranthiencdsp@gmail.com>
2016-10-17 16:40:42 +0700

    Add DD Proxy

---
+++ ConsoleAppWithGitHub/ConsoleApp/DDProxy/IMath.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace DDProxy
+{
+    public interface IMath
+    {
+        double Add(double x, double y);
+
+        double Sub(double x, double y);
+
+        double Mul(double x, double y);
+
+        double Div(double x, double y);
+    }
+}
---
+++ ConsoleAppWithGitHub/ConsoleApp/DDProxy/Math.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+public class Math : IMath
+{
+    public double Add(double x, double y)
+    {
+        return x + y;
+    }
+
+    public double Div(double x, double y)
+    {
+        if (y == 0)
+            throw new DivideByZeroException();
+
+        return x / y;
+    }
+
+    public double Mul(double x, double y)
+    {
+        return x * y;
+    }
+
+    public double Sub(double x, double y)
+    {
+        return x - y;
+    }
+}
